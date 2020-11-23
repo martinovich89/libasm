0d5e7e37ab3d0c4ccc8069f02c18dc3e1e3345ac
mhenry <mhenry@student.42.fr>
2020-11-23 19:46:38 +0100

    added ft_read prototype to libasm.h

--- libasm.h
+++ libasm.h
@@ -14,5 +14,6 @@ int		ft_strcmp(char *s1, char *s2);
 char	*ft_strcpy(char *dest, char *src);
 char	*ft_strdup(char *str);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, const void *buf, size_t count);
 
 #endif
